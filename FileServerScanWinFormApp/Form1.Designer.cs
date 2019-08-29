92e343fb4e7ebc39b7ed1be1caceb986602ddec2
Wallyson Silva <wallysonferreirasilva@gmail.com>
2019-08-29 18:04:02 -0300

    Criação da janela principal e adaptando código de procurar palavra.

--- FileServerScanWinFormApp/Form1.Designer.cs
+++ FileServerScanWinFormApp/Form1.Designer.cs
@@ -28,13 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblDiretorio = new System.Windows.Forms.Label();
+            this.lblPalavra = new System.Windows.Forms.Label();
+            this.txtDiretorio = new System.Windows.Forms.TextBox();
+            this.txtPalavra = new System.Windows.Forms.TextBox();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.lstBxListaArquivos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblDiretorio
+            // 
+            this.lblDiretorio.AutoSize = true;
+            this.lblDiretorio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiretorio.Location = new System.Drawing.Point(8, 9);
+            this.lblDiretorio.Name = "lblDiretorio";
+            this.lblDiretorio.Size = new System.Drawing.Size(155, 20);
+            this.lblDiretorio.TabIndex = 0;
+            this.lblDiretorio.Text = "Caminho do diretório";
+            // 
+            // lblPalavra
+            // 
+            this.lblPalavra.AutoSize = true;
+            this.lblPalavra.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPalavra.Location = new System.Drawing.Point(15, 39);
+            this.lblPalavra.Name = "lblPalavra";
+            this.lblPalavra.Size = new System.Drawing.Size(137, 20);
+            this.lblPalavra.TabIndex = 1;
+            this.lblPalavra.Text = "Palavra procurada";
+            // 
+            // txtDiretorio
+            // 
+            this.txtDiretorio.Location = new System.Drawing.Point(169, 9);
+            this.txtDiretorio.Name = "txtDiretorio";
+            this.txtDiretorio.Size = new System.Drawing.Size(389, 20);
+            this.txtDiretorio.TabIndex = 2;
+            // 
+            // txtPalavra
+            // 
+            this.txtPalavra.Location = new System.Drawing.Point(169, 40);
+            this.txtPalavra.Name = "txtPalavra";
+            this.txtPalavra.Size = new System.Drawing.Size(389, 20);
+            this.txtPalavra.TabIndex = 3;
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.AutoSize = true;
+            this.btnPesquisar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPesquisar.Location = new System.Drawing.Point(575, 21);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(84, 28);
+            this.btnPesquisar.TabIndex = 4;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // lstBxListaArquivos
+            // 
+            this.lstBxListaArquivos.FormattingEnabled = true;
+            this.lstBxListaArquivos.Location = new System.Drawing.Point(12, 204);
+            this.lstBxListaArquivos.Name = "lstBxListaArquivos";
+            this.lstBxListaArquivos.Size = new System.Drawing.Size(650, 251);
+            this.lstBxListaArquivos.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(674, 471);
+            this.Controls.Add(this.lstBxListaArquivos);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.txtPalavra);
+            this.Controls.Add(this.txtDiretorio);
+            this.Controls.Add(this.lblPalavra);
+            this.Controls.Add(this.lblDiretorio);
+            this.Name = "Form1";
+            this.Text = "TP Scan File Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblDiretorio;
+        private System.Windows.Forms.Label lblPalavra;
+        private System.Windows.Forms.TextBox txtDiretorio;
+        private System.Windows.Forms.TextBox txtPalavra;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.ListBox lstBxListaArquivos;
     }
 }
 
--- FileServerScanWinFormApp/Form1.cs
+++ FileServerScanWinFormApp/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace FileServerScanWinFormApp
 {
@@ -16,5 +18,51 @@ namespace FileServerScanWinFormApp
         {
             InitializeComponent();
         }
+
+        bool contemPalavra = false;
+       
+        public void EncontraPalavra()
+        {   
+            var arquivos = Directory.EnumerateFiles(txtDiretorio.Text, "*.*", SearchOption.AllDirectories);
+
+            foreach (string arquivoAtual in arquivos)
+            {
+                PercorreArquivo(arquivoAtual);
+            }         
+
+        }
+
+        public void PercorreArquivo(string ArquivoAtual)
+        {
+            StreamReader sr;
+            sr = File.OpenText(ArquivoAtual);
+            while (!sr.EndOfStream)
+            {
+                string linhaDoArquivo = sr.ReadLine();
+                foreach (Match match in Regex.Matches(linhaDoArquivo, txtPalavra.Text, RegexOptions.IgnoreCase))
+                {
+                    contemPalavra = true;
+                    break;
+                }
+            }
+            if (contemPalavra)
+            {
+                lstBxListaArquivos.Items.Add(ArquivoAtual);
+                contemPalavra = false;
+            }
+            sr.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            EncontraPalavra();           
+        }
+
+        
     }
 }
